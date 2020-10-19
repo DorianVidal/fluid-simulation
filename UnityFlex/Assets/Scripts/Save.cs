f5f0b96945c7d947cae5a8bf0dde49f06a37b38b
DorianVidal <dorianpierrickvidal@gmail.com>
2020-10-19 09:06:29 +0200

    0.0.8

--- UnityFlex/Assets/Scripts/Save.cs
+++ UnityFlex/Assets/Scripts/Save.cs
@@ -1,21 +1,31 @@
 ﻿using UnityEngine;
 using System.IO;
 using NVIDIA.Flex;
+using System;
+using System.Collections;
+using System.Diagnostics;
+using UnityScript.Scripting.Pipeline;
 
 /*
  * Auteur : Dorian Vidal 
  * 
  * Ce script permet la sauvegarde en local des variables d'un preset
+ *
+ * Utilisation : Creer un empty Game Object et lui ajouter le script
  */
 
 
 public class Save : MonoBehaviour
 {
     public FlexContainer flex;
-    public float Cohesion;
-    public float tension;
-    public float viscosity;
-    public float adhesion;
+
+    private float Cohesion;
+    private float tension;
+    private float viscosity;
+    private float adhesion;
+
+    private string saveSeparator = "%VALUE%";//Cette valeur est un separateur de données, pour éviter de corrompre une donnée, on va 
+    //ArrayList<float>[4](Cohesion, tension, viscosity; adhesion);
 
     void SaveDatas()
     {
@@ -23,11 +33,31 @@ public class Save : MonoBehaviour
         tension = flex.surfaceTension;
         viscosity = flex.viscosity;
         adhesion = flex.adhesion;
-        File.WriteAllText(Application.dataPath + "/data.txt", Cohesion, tension, viscosity, adhesion); //Viens creer dans le dossier assets un fichier avec toutes les donées sauvegardées
+
+        string[] content = new string[]
+        {
+            Cohesion.ToString(), 
+            tension.ToString(),
+            viscosity.ToString(),
+            adhesion.ToString()
+        };
+
+        string saveString = string.Join(saveSeparator, content);
+
+        File.WriteAllText(Application.dataPath + "/data.txt", saveString); //Viens creer dans le dossier assets un fichier avec toutes les donées sauvegardées
+        
     }
 
     void LoadDatas()
     {
+        string saveString = File.ReadAllText(Application.dataPath + "/data.txt");
+
+        string[] content = saveString.Split(new[] { saveSeparator }, System.StringSplitOptions.None);
+
+        Cohesion = float.Parse(content[0]);
+        tension = float.Parse(content[1]);
+        viscosity = float.Parse(content[2]);
+        adhesion = float.Parse(content[3]);
 
     }
 
@@ -39,7 +69,12 @@ public class Save : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.L))
         {
-            SaveDatas();
+            LoadDatas();
+            flex.cohesion = Cohesion;
+            flex.surfaceTension = tension;
+            flex.viscosity = viscosity;
+            flex.adhesion = adhesion;
+
         }
 
     }
