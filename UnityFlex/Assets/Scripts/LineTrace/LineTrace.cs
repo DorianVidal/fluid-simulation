6f4cacfd4afab48df5dc906f2d820f9b6872566a
Dorian <VIDAL>
2020-11-03 12:06:42 +0100

    [update] LineSystemBegin
    Je commence le line system, problème pour récupérer une référence de la particule

---
+++ UnityFlex/Assets/Scripts/LineTrace/LineTrace.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NVIDIA;
+using NVIDIA.Flex;
+
+public class LineTrace : MonoBehaviour
+{
+    public FlexContainer FCReference;
+    FlexFluidRenderer fluidReference;
+    public TrailRenderer TrailReference;
+    [SerializeField]
+    public FlexContainer.ParticleData ParticlesDatas;
+    private TrailRenderer trail;
+    private Vector4 particle;
+
+
+   
+
+    private void Update()
+    {
+        ParticlesDatas =  FCReference.m_particleData;
+        //ParticulesDatas.GetParticle(1);
+        if(ParticlesDatas.GetParticle(1) == null)
+        {
+            particle = ParticlesDatas.GetParticle(1);
+            trail = Instantiate(TrailReference);            
+        }
+        else
+        {
+            trail.transform.position = particle;
+        }
+    }
+
+}
