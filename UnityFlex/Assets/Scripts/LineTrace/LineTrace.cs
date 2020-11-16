7eded952a6cc8af43440d5759182d2de477816c2
Dorian <VIDAL>
2020-11-16 15:09:32 +0100

    [ADD + UPDATE] AJout d'une nouvelle aort et correction de bug

---
+++ UnityFlex/Assets/Scripts/LineTrace/LineTrace.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NVIDIA.Flex;
+
+public class LineTrace : MonoBehaviour
+{
+    List<Vector3> ParticulePosArray;
+    public FlexContainer container;
+    public FlexContainer.ParticleData particleData;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetParticlesPos(particleData);
+    }
+    void GetParticlesPos(FlexContainer.ParticleData particle)
+    {
+        print(particle.GetParticle(container.fluidIndices[1]));
+        //for(int i = 0; i < flexContainer.count)
+        //print(flexContainer.fluidIndices);
+        
+        //flexContainer.FreeParticles(flexContainer.fluidIndices);
+    }
+}
