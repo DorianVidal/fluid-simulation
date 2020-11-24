7170a1418f9f19edc7f74d1c1df54418db79f034
Dorian <VIDAL>
2020-11-24 16:45:08 +0100

    [UPDATE + INTEGRATION ]
    Ajout des streams lines

---
+++ UnityFlex/Assets/Scripts/DrawLinesParticles.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NVIDIA.Flex;
+
+public class DrawLinesParticles : MonoBehaviour
+{
+    public FlexContainer fcContainer;
+    public FlexSourceActor fsaFlexSourceActor;
+    public GameObject goLineTraceByParticle;
+    public int iNumberLines;
+    private float fTimeBetweenPos = 0.1f;
+    public int iParticleToSkip;
+    public int iParticuleMeshNumber; //Fais référence au nombre de particules dans le mesh
+    // Start is called before the first frame update
+    void Awake()
+    {
+        for (int i = 1; i <= iNumberLines; i++)
+        {
+            GameObject instance = Instantiate(goLineTraceByParticle);
+            instance.transform.parent = gameObject.transform;
+            instance.GetComponent<ParticlePos>().SetValues(fcContainer, fsaFlexSourceActor, fTimeBetweenPos, iParticleToSkip*i + iParticuleMeshNumber);           
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print("NBParticule: " + fcContainer.m_particleArray.Length);
+        //print("NBFluidParticule: " + fcContainer.m_fluidIndices.Length);
+    }
+}
---
+++ UnityFlex/Assets/Scripts/ParticlePos.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NVIDIA.Flex;
+
+public class ParticlePos : MonoBehaviour
+{
+    public FlexContainer fcContainer;
+    public LineRenderer lrLineRenderer;
+    public float fTimer;
+    public int iBeforeLastParticle;
+    private float fChrono = 0;
+    public FlexSourceActor fsaFlexSourceActor;
+    int i;
+
+    private void Start()
+    {
+        lrLineRenderer = GetComponent<LineRenderer>();
+        lrLineRenderer.positionCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fChrono -= Time.deltaTime;
+        if (fChrono <= 0)
+        {
+            if (fcContainer.m_particleArray[fcContainer.m_particleArray.Length - iBeforeLastParticle] != Vector4.zero && lrLineRenderer.positionCount < fsaFlexSourceActor.lifeTime*8)
+            {
+                lrLineRenderer.positionCount = lrLineRenderer.positionCount + 1;
+                lrLineRenderer.SetPosition(i, fcContainer.m_particleArray[fcContainer.m_particleArray.Length - iBeforeLastParticle]);
+                i++;
+            }
+            fChrono = fTimer;
+        }
+    }
+
+    public void SetValues(FlexContainer container, FlexSourceActor flexsourceactor , float timer, int beforelastparticle)
+    {
+        fcContainer = container;
+        fsaFlexSourceActor = flexsourceactor;
+        fTimer = timer;
+        iBeforeLastParticle = beforelastparticle;
+    }
+}
